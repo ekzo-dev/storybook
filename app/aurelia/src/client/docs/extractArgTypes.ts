2379e0a925652464a5e0c9193b8dd6723ddbfae1
Архипов Дмитрий <da@netcitylife.ru>
2022-08-01 10:24:44 +0200

    Disable falsy arg controls

--- app/aurelia/src/client/docs/extractArgTypes.ts
+++ app/aurelia/src/client/docs/extractArgTypes.ts
@@ -2,6 +2,13 @@ import type { StrictArgTypes } from '@storybook/csf';
 import type { ArgTypesExtractor } from '@storybook/docs-tools';
 import { CustomElement } from 'aurelia';
 
+// declare namespace Reflect {
+//   // eslint-disable-next-line @typescript-eslint/no-explicit-any
+//   function getMetadata(metadataKey: any, target: any, propertyKey?: string | symbol): any;
+// }
+// const metadata = Reflect.getMetadata('design:type', ButtonCustomElement, bindable.property);
+// console.log(metadata);
+
 export const extractArgTypes: ArgTypesExtractor = (component) => {
   if (component) {
     const def = CustomElement.getDefinition(component);
@@ -9,9 +16,9 @@ export const extractArgTypes: ArgTypesExtractor = (component) => {
       acc[bindable.attribute] = {
         name: bindable.attribute,
         description: bindable.property,
-        control: {
-          type: 'text',
-        },
+        // control: {
+        //   type: 'text',
+        // },
       };
 
       return acc;
